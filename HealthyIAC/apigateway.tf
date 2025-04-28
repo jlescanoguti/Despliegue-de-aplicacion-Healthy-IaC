30ba3456b6aff01ef5db0180bacd539c7d333dfe
Megalo04 <malcaldel1@upao.edu.pe>
2025-04-27 23:20:34 -0500

    prueba apigateway con hola mundo

--- HealthyIAC/apigateway.tf
+++ HealthyIAC/apigateway.tf
@@ -1,3 +1,4 @@
+# 1. API Gateway
 resource "aws_apigatewayv2_api" "healthy_api" {
   name          = "healthy-api"
   protocol_type = "HTTP"
@@ -11,4 +12,28 @@ resource "aws_apigatewayv2_stage" "healthy_api_stage" {
 
 output "api_gateway_url" {
   value = aws_apigatewayv2_stage.healthy_api_stage.invoke_url
+}
+
+# 2. Integración con Lambda
+resource "aws_apigatewayv2_integration" "api_integration" {
+  api_id                 = aws_apigatewayv2_api.healthy_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.hello_world.invoke_arn
+  payload_format_version = "2.0"
+}
+
+# 3. Ruta para invocar la Lambda
+resource "aws_apigatewayv2_route" "api_route" {
+  api_id    = aws_apigatewayv2_api.healthy_api.id
+  route_key = "GET /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.api_integration.id}"
+}
+
+# 4. Permiso para que API Gateway invoque la Lambda
+resource "aws_lambda_permission" "api_gateway_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_world.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.healthy_api.execution_arn}/*/*"
 }
